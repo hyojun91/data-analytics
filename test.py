cf68a85ae817328f0ad22a36641f29a9b9736183
hyojun91 <khjoon7@gmail.com>
2021-01-24 11:26:07 +0900

    add print

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
 print("hello world")
+print("Data Science")
+def 
+# vscode-insiders://vscode.github-authentication/did-authenticate?windowid=1&code=b4e763eda646f9d3ad7f&state=aca77590-4bfe-4f4b-a04b-31fa641cf3ba
